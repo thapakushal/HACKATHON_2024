d27da0ab6fd87e4f6204325ce5b36ef039a14b94
Kushal Thapa Magar <kushalmagar007@gmail.com>
2024-11-21 14:24:16 +0545

    added login

--- Hola-Amigos/resources/views/components/header.blade.php
+++ Hola-Amigos/resources/views/components/header.blade.php
@@ -1,12 +1,10 @@
-
-
 <header class="text-white body-font bg-[#21A350]">
     <div class="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
         <a class="flex items-center mb-4 font-medium title-font md:mb-0">
             <img 
                 alt="Logo" 
                 class="block object-contain h-12 p-2 w-13" 
-                src="{{ asset('images\logo.png') }}">
+                src="{{ asset('images/logo.png') }}">
             <span class="ml-3 text-xl">Mal Basket</span>
         </a>
         <nav class="flex flex-wrap items-center justify-center text-base md:ml-auto">
@@ -14,12 +12,14 @@
             <a class="mr-5 hover:text-gray-300">About Us</a>
             <a class="mr-5 hover:text-gray-300">Contact Us</a>
         </nav>
-        <button class="inline-flex items-center px-3 py-1 mt-4 text-base text-black bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0">
-            Login
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-        </button>
-        
+        <!-- Link to the login page -->
+        <a href="{{ route('login') }}">
+            <button class="inline-flex items-center px-3 py-1 mt-4 text-base text-black bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0">
+                Login
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+            </button>
+        </a>
     </div>
 </header>
